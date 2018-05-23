ac6dfa953b97b6b8413e71045c84b045b6eeee69
pradel <pradel.leo@gmail.com>
2018-05-23 15:14:54 +0200

    update enzyme and react

--- test/portal.js
+++ test/portal.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { assert } from 'chai';
 import Portal from '../src/portal';
+
+configure({ adapter: new Adapter() });
 
 describe('Portal', () => {
   it('should not be in current react three', () => {
     const element = <p>Hello</p>;
-    const wrapper = shallow(<Portal>{element}</Portal>);
+    const wrapper = mount(<Portal>{element}</Portal>);
     assert.equal(wrapper.find(element).length, 0);
     wrapper.unmount();
   });
 
   it('should be rendered in a new react three', () => {
     const element = <p>Hello</p>;
-    const wrapper = shallow(<Portal>{element}</Portal>);
+    const wrapper = mount(<Portal>{element}</Portal>);
     assert.match(document.documentElement.innerHTML, /Hello/);
     wrapper.unmount();
   });
@@ -30,7 +33,7 @@ describe('Portal', () => {
 
   it('should unmount', () => {
     const element = <p>Hello</p>;
-    const wrapper = shallow(<Portal>{element}</Portal>);
+    const wrapper = mount(<Portal>{element}</Portal>);
     assert.match(document.documentElement.innerHTML, /Hello/);
     wrapper.unmount();
     assert.notMatch(document.documentElement.innerHTML, /Hello/);
