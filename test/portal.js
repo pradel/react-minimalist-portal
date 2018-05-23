d0dac48f76ba0149b961c574cd8ce0e1e0855b6d
pradel <pradel.leo@gmail.com>
2018-05-23 15:43:28 +0200

    update jsdom

--- test/portal.js
+++ test/portal.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
 import { assert } from 'chai';
 import Portal from '../src/portal';
-
-configure({ adapter: new Adapter() });
 
 describe('Portal', () => {
   it('should not be in current react three', () => {
--- test/setup.js
+++ test/setup.js
@@ -1,21 +1,26 @@
-import { configure, mount } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import jsdomLib from 'jsdom';
+import { JSDOM } from 'jsdom';
 
 configure({ adapter: new Adapter() });
 
-const jsdom = jsdomLib.jsdom;
 const exposedProperties = ['window', 'navigator', 'document'];
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
 
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+}
 
+global.window = window;
+global.document = window.document;
 global.navigator = {
   userAgent: 'node.js',
 };
+copyProps(window, global);
